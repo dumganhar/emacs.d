f17898f13d19d46842c7a87a8553f5510207f7f6
James Chen <jianhua.chen@cocos2d-x.org>
2014-03-26 20:41:42 +0800

    Fix chinese font isn't monospaced.

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -5,6 +5,20 @@
 (require 'linum)
 (global-linum-mode t)
 
+(when (display-graphic-p)
+  (setq fonts
+        (cond ((eq system-type 'darwin)     '("Monaco"     "STHeiti"))
+              ((eq system-type 'gnu/linux)  '("Menlo"     "WenQuanYi Zen Hei"))
+              ((eq system-type 'windows-nt) '("Consolas"  "Microsoft Yahei"))))
+
+  (setq face-font-rescale-alist '(("STHeiti" . 1.2) ("Microsoft Yahei" . 1.2) ("WenQuanYi Zen Hei" . 1.2)))
+  (set-face-attribute 'default nil :font
+                      (format "%s:pixelsize=%d" (car fonts) 14))
+  (dolist (charset '(kana han symbol cjk-misc bopomofo))
+    (set-fontset-font (frame-parameter nil 'font) charset
+                      (font-spec :family (car (cdr fonts))))))
+
+
 (provide 'init-local)
 ;;;
 ;;;
