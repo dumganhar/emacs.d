fa6d1d691c26c3cce0a9f2d5e5d1dfc6ce36cfe9
James Chen <jianhua.chen@cocos2d-x.org>
2014-03-27 23:37:03 +0800

    Binding key 'M-[', 'M-]' for indention.

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -6,6 +6,7 @@
 (global-linum-mode t)
 (setq linum-format "%3d ")
 
+;; Set monospaced fonts for english and chinese
 (when (display-graphic-p)
   (setq fonts
         (cond ((eq system-type 'darwin)     '("Monaco"     "STHeiti"))
@@ -19,6 +20,28 @@
     (set-fontset-font (frame-parameter nil 'font) charset
                       (font-spec :family (car (cdr fonts))))))
 
+;; Define indention key
+;;; It's kind of sad this doesn't exist normally...
+    (defun indent-rigidly-n (n)
+      "Indent the region, or otherwise the current line, by N spaces."
+      (let* ((use-region (and transient-mark-mode mark-active))
+             (rstart (if use-region (region-beginning) (point-at-bol)))
+             (rend   (if use-region (region-end)       (point-at-eol)))
+             (deactivate-mark "irrelevant")) ; avoid deactivating mark
+        (indent-rigidly rstart rend n)))
+    (defun indent-rigidly-4 ()
+      "Indent the region, or otherwise the current line, by 4 spaces."
+      (interactive)
+      (indent-rigidly-n 4))
+    (defun outdent-rigidly-4 ()
+      "Indent the region, or otherwise the current line, by -4 spaces."
+      (interactive)
+      (indent-rigidly-n -4))
+
+    (global-set-key (kbd "M-]") 'indent-rigidly-4)
+    (global-set-key (kbd "M-[") 'outdent-rigidly-4)
+
+;;;
 
 (provide 'init-local)
 ;;;
